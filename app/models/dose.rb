7672425348e5e47a0ee3b6db7697fa016f0ad619
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-11 12:22:48 +0300

    Created Models

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
+  validates :cocktail, uniqueness: { scope: :ingredient }
+end
+
